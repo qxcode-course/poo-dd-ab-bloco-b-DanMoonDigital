9324a38a3e58dbafb9dde15bbc8379b4c4e8e04f
Dan <pedrodanielmota@gmail.com>
2025-10-20 14:57:00 -0300

    camisas estilosas

---
+++ myrep/poo/camisa/py/draft.py
@@ -0,0 +1,22 @@
+class Camisa:
+    def __init__(self):
+        self.tamanho = 0
+
+    def getTamanho(self):
+        return self.tamanho
+    
+    def setTamanho(self, valor: int):
+        tamanho_validos = ["PP", "P", "M", "G", "GG", "XG"]
+        if valor in tamanho_validos:
+            self.tamanho = valor
+        else:
+            print("Valor inválido, tente PP, P, M, G, GG ou XG")
+
+camisa = Camisa()
+
+while camisa.getTamanho() == 0:
+    print("Digite seu tamanho de Roupa:")
+    tamanho = input ()
+    camisa.setTamanho(tamanho)
+
+print("Parabéns, você comporu uma roupa tamanho")
